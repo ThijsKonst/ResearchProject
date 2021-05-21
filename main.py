23b527175e3cf51cc81130b65277472c3f8a2c63
ThijsKonst <thijskonst@gmail.com>
2021-05-21 15:19:39 +0200

    Made a lot of progress and added custom attention mechanism

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
-from tensorflow.keras import Input, optimizers, regularizers, callbacks
-from tensorflow.keras.layers import Dense, LSTM, Dropout
-from tensorflow.keras.models import load_model, Model, Sequential
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import os
+import matplotlib
+import time
+import ModelTrunk
+
+from matplotlib import pyplot as plt
+from tensorflow.keras.layers import Dense, LSTM, Input
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.callbacks import LearningRateScheduler
 from sklearn.preprocessing import MinMaxScaler
 from math import sqrt
 from sklearn.metrics import mean_squared_error
 
-import matplotlib
 matplotlib.use('TkAgg')
-from matplotlib import pyplot as plt
-
-import seaborn as sns
-import pandas as pd
-import numpy as np
-import datetime as dt
-import os
 
 checkpoint_path = "models/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
-def main():
+attention = True
+gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+for device in gpu_devices:
+    tf.config.experimental.set_memory_growth(device, True)
 
-    ## LOADING AND SCALING DATA ##
-    dataframe = pd.read_csv("data/joined_data_2015-2018.csv", parse_dates = ['date'],index_col=1)
-    testdataframe = pd.read_csv("data/test_data_2018-2019.csv", index_col=1)
+
+def main():
+    # LOADING AND SCALING DATA ##
+    dataframe = pd.read_csv("data/joined_data_2015-2019.csv",
+                            parse_dates=['date'], index_col=1)
 
     dates = dataframe.index
     ref_forecasts = dataframe['DAF']
     dataframe = dataframe.reset_index()
     dataframe['WD'] = dataframe['date'].dt.dayofweek
-    variables = dataframe[["HH", "WD", "FH", "DD", "T", "AT"]]
+    dataframe['MM'] = dataframe['date'].dt.month
+    variables = dataframe[["HH", "WD", "MM", "FH", "DD", "T", "DAP", "AT"]]
     columns = list(variables)
     df = dataframe[columns].values.astype('float32')
 
-    n_train_hours = 130 * 168
+    n_train_hours = 35064 - 8784
 
     df_train = df[:n_train_hours, :]
     df_test = df[n_train_hours:, :]
@@ -41,7 +49,7 @@ def main():
     train = scaler.transform(df_train)
     test = scaler.transform(df_test)
 
-    ## TRAINING ##
+    # TRAINING
     train_X, train_Y = train[:, :-1], train[:, -1]
     test_X, test_Y = test[:, :-1], test[:, -1]
 
@@ -49,49 +57,104 @@ def main():
     test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
     print(train_X.shape, train_Y.shape, test_X.shape, test_Y.shape)
 
+    # MODEL
 
-    ## MODEL ##
+    if attention:
+        inp = Input(shape=(train_X.shape[1], train_X.shape[2]))
+        x = ModelTrunk.ModelTrunk(num_layers=4, dropout=0.1)(inp)
+        x = Dense(1)(x)
+        x = Dense(1)(x)
 
-    cp_callback = callbacks.ModelCheckpoint(filepath = checkpoint_path, save_weights_only=True, verbose=1)
+        model = Model(inp, x)
+        model.compile(optimizer='adam', loss='mse')
+    else:
+        model = Sequential()
+        model.add(LSTM(64, input_shape=(train_X.shape[1], train_X.shape[2],),
+                  return_sequences=True))
+        model.add(LSTM(32))
+        model.add(Dense(1))
 
-    model = Sequential()
-    model.add(LSTM(64, input_shape=(train_X.shape[1], train_X.shape[2],), return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(LSTM(32, activation='relu', return_sequences=False))
-    model.add(Dropout(0.2))
-    model.add(Dense(1))
+        model.compile(optimizer='rmsprop', loss='mse')
 
-    model.compile(optimizer='adam', loss='mae')
     model.summary()
 
-    history = model.fit(train_X, train_Y, epochs=500, batch_size=24,
-                        validation_data = (test_X, test_Y),
-                        verbose=2, shuffle=False, callbacks=[cp_callback])
+    def lr_scheduler(epoch, lr, warmup_epochs=15, decay_epochs=150, initial_lr=1e-6, base_lr=1e-3, min_lr=5e-5):
+        if epoch <= warmup_epochs:
+            pct = epoch / warmup_epochs
+            return ((base_lr - initial_lr) * pct) + initial_lr
 
+        if epoch > warmup_epochs and epoch < warmup_epochs+decay_epochs:
+            pct = 1 - ((epoch - warmup_epochs) / decay_epochs)
+            return ((base_lr - min_lr) * pct) + min_lr
+
+        return min_lr
+
+    callbacks = [LearningRateScheduler((lr_scheduler), verbose=0)]
+
+    history = model.fit(train_X, train_Y, epochs=200, batch_size=24,
+              validation_data=(test_X, test_Y), verbose=1, shuffle=False, callbacks=callbacks)
+    if not attention:
+        model.save("models/lstm.h5")
+
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
 
     forecast = model.predict(test_X)
 
     test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
 
-    inv_forecast = np.concatenate((test_X[:, -5:], forecast), axis=1)
+    print(forecast.shape)
+    inv_forecast = np.concatenate((test_X[:, -7:], forecast), axis=1)
+    print(inv_forecast.shape)
     inv_forecast = scaler.inverse_transform(inv_forecast)
-    inv_forecast = inv_forecast[:,5]
+    inv_forecast = inv_forecast[:, 7]
 
     test_Y = test_Y.reshape((len(test_Y), 1))
-    inv_y = np.concatenate((test_X[:, -5:], test_Y), axis=1)
+    inv_y = np.concatenate((test_X[:, -7:], test_Y), axis=1)
     inv_y = scaler.inverse_transform(inv_y)
-    inv_y = inv_y[:,5]
+    inv_y = inv_y[:, 7]
 
     rmse = sqrt(mean_squared_error(inv_y, inv_forecast))
-    reference_rmse = sqrt(mean_squared_error(inv_y, ref_forecasts[n_train_hours:]))
-    print('Test RMSE: %.3f' % rmse)
-    print('Reference RMSE: %.3f' % reference_rmse)
+    reference_rmse = sqrt(mean_squared_error(inv_y,
+                                             ref_forecasts[n_train_hours:]))
 
-    dataframe_graph = pd.DataFrame({'date' : dates[n_train_hours:], 'real' : inv_y, 'forecast' : inv_forecast, 'ref_forecast' : ref_forecasts[n_train_hours:]})
+    currentTime = str(int(time.time()))
+    if attention:
+        f = open("results/Test_results_attention_" + currentTime,
+                 "x")
+    else:
+        f = open("results/Test_results_LSTM_" + currentTime, "x")
 
-    sns.lineplot(x= dataframe_graph['date'], y=dataframe_graph['real'], legend="full", label="Real [MW]")
-    sns.lineplot(x= dataframe_graph['date'], y=dataframe_graph['forecast'], legend="full", label="Forecast [MW]")
-    sns.lineplot(x= dataframe_graph['date'], y=dataframe_graph['ref_forecast'], legend="full", label="ref_Forecast [MW]")
+    print("written at " + currentTime)
+    f.write('Test RMSE: %.3f \n' % rmse)
+    f.write('Reference RMSE: %.3f \n' % reference_rmse)
+
+    dataframe_graph = pd.DataFrame({'date': dates[n_train_hours:],
+                                    'real': inv_y, 'forecast': inv_forecast,
+                                    'ref_forecast':
+                                    ref_forecasts[n_train_hours:]})
+
+    f.write(dataframe_graph.describe().to_string())
+    f.close()
+    sns.lineplot(x=dataframe_graph['date'], y=dataframe_graph['real'],
+                 legend="full", label="Real [MW]")
+    sns.lineplot(x=dataframe_graph['date'], y=dataframe_graph['forecast'],
+                 legend="full", label="Forecast [MW]")
+    sns.lineplot(x=dataframe_graph['date'], y=dataframe_graph['ref_forecast'],
+                 legend="full", label="ref_Forecast [MW]")
     plt.show()
 
 
--- makeCSV.py
+++ makeCSV.py
@@ -1,70 +1,49 @@
 from pandas import read_csv
 from datetime import datetime, timedelta
 
+
 def parser(x, y):
     if y == "24":
         y = "0"
         return datetime.strptime(x + y, '%Y%m%d%H') + timedelta(days=1)
     return datetime.strptime(x + y, '%Y%m%d%H')
 
-energyframe = read_csv('data/2015-2016.csv').append(read_csv('data/2016-2017.csv')).append(read_csv('data/2017-2018.csv'))
+
+energyframe = read_csv('data/2015-2016.csv').append(read_csv('data/2016-2017.csv')).append(read_csv('data/2017-2018.csv')).append(read_csv('data/2018-2019.csv'))
 # Removing daylight saving hours from dataset
 energyframe = energyframe.dropna()
-energyframe.reset_index(inplace = True, drop=True)
+energyframe.reset_index(inplace=True, drop=True)
 
 # Group 4 times 15 minutes into an hour
 energyframe = energyframe.groupby(energyframe.index // 4).sum()
 print(energyframe)
 
+priceframe = read_csv('data/prices2015-2016.csv').append(read_csv('data/prices2016-2017.csv')).append(read_csv('data/prices2017-2018.csv')).append(read_csv('data/prices2018-2019.csv'))
+priceframe = priceframe.drop("BZN|NL", axis=1)
+priceframe.reset_index(inplace=True, drop=True)
+priceframe = priceframe.fillna(priceframe.mean(axis=0))
+
+print(priceframe)
 
 dataset = read_csv("data/uurgeg_260_2011-2020.csv",
-                   parse_dates={"date" : ['YYYYMMDD','HH']}, keep_date_col=True,
+                   parse_dates={"date": ['YYYYMMDD', 'HH']},
+                   keep_date_col=True,
                    date_parser=parser)
 
 dataset = dataset.drop("STN", axis=1)
 
-dataset.index.name ='date'
+dataset.index.name = 'date'
 # Only selecting the needed years of data
 mask = (dataset['date'] > '2015-01-01') & (dataset['date'] <
-                                           '2018-01-01 01:00:00')
-
-dataset = dataset[mask]
-dataset.reset_index(inplace = True, drop=True)
-
-print(dataset)
-
-# Merging the two datasets :)
-dataset = dataset.merge(energyframe[["AT","DAF"]], left_index=True, right_index=True)
-
-dataset.to_csv("data/joined_data_2015-2018.csv")
-
-energyframe = read_csv('data/2018-2019.csv')
-# Removing daylight saving hours from dataset
-energyframe = energyframe.dropna()
-energyframe.reset_index(inplace = True, drop=True)
-
-# Group 4 times 15 minutes into an hour
-energyframe = energyframe.groupby(energyframe.index // 4).sum()
-print(energyframe)
-
-
-dataset = read_csv("data/uurgeg_260_2011-2020.csv",
-                   parse_dates={"date" : ['YYYYMMDD','HH']},
-                   date_parser=parser)
-
-dataset = dataset.drop("STN", axis=1)
-
-dataset.index.name ='date'
-# Only selecting the needed years of data
-mask = (dataset['date'] > '2018-01-01') & (dataset['date'] <
                                            '2019-01-01 01:00:00')
 
 dataset = dataset[mask]
-dataset.reset_index(inplace = True, drop=True)
+dataset.reset_index(inplace=True, drop=True)
 
 print(dataset)
 
 # Merging the two datasets :)
 dataset = dataset.merge(energyframe[["AT","DAF"]], left_index=True, right_index=True)
+dataset = dataset.merge(priceframe[["Day-ahead Price [EUR/MWh]"]], left_index=True, right_index=True)
 
-dataset.to_csv("data/test_data_2018-2019.csv")
+dataset.to_csv("data/joined_data_2015-2019.csv")
