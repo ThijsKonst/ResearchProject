f73bc24b88fad5721596e6bc4c44cc0fcbb5c70c
ThijsKonst <thijskonst@gmail.com>
2021-06-18 04:06:27 +0200

    Added progress

--- ModelTrunk.py
+++ ModelTrunk.py
@@ -1,4 +1,5 @@
 import keras
+import Time2Vec
 from keras import backend as K
 from tensorflow_addons.layers import MultiHeadAttention
 
@@ -8,7 +9,7 @@ from tensorflow_addons.layers import MultiHeadAttention
 class ModelTrunk(keras.Model):
     def __init__(self, name='ModelTrunk', time2vec_dim=1, num_heads=2, head_size=128, ff_dim=None, num_layers=1, dropout=0, **kwargs):
         super().__init__(name=name, **kwargs)
-        self.time2vec = Time2Vec(kernel_size=time2vec_dim)
+        self.time2vec = Time2Vec.Time2Vec(kernel_size=time2vec_dim)
         if ff_dim is None:
             ff_dim = head_size
         self.dropout = dropout
@@ -53,29 +54,3 @@ class AttentionBlock(keras.Model):
         x = self.ff_norm(inputs + x)
         return x
 
-
-class Time2Vec(keras.layers.Layer):
-    def __init__(self, kernel_size=1):
-        super(Time2Vec, self).__init__(trainable=True, name='Time2VecLayer')
-        self.k = kernel_size
-
-    def build(self, input_shape):
-        # trend
-        self.wb = self.add_weight(name='wb',shape=(input_shape[1],),initializer='uniform',trainable=True)
-        self.bb = self.add_weight(name='bb',shape=(input_shape[1],),initializer='uniform',trainable=True)
-        # periodic
-        self.wa = self.add_weight(name='wa',shape=(1, input_shape[1], self.k),initializer='uniform',trainable=True)
-        self.ba = self.add_weight(name='ba',shape=(1, input_shape[1], self.k),initializer='uniform',trainable=True)
-        super(Time2Vec, self).build(input_shape)
-
-    def call(self, inputs, **kwargs):
-        bias = self.wb * inputs + self.bb
-        dp = K.dot(inputs, self.wa) + self.ba
-        wgts = K.sin(dp)  # or K.cos(.)
-
-        ret = K.concatenate([K.expand_dims(bias, -1), wgts], -1)
-        ret = K.reshape(ret, (-1, inputs.shape[1]*(self.k+1)))
-        return ret
-
-    def compute_output_shape(self, input_shape):
-        return (input_shape[0], input_shape[1]*(self.k + 1))
--- main.py
+++ main.py
@@ -2,22 +2,28 @@ import seaborn as sns
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-import os
 import matplotlib
 import time
 import ModelTrunk
+import peel_the_layer
+
+import keras.applications
+import keras.datasets
+import keras.preprocessing
+import keras.wrappers
+
 
 from matplotlib import pyplot as plt
-from tensorflow.keras.layers import Dense, LSTM, Input, Dropout
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.callbacks import LearningRateScheduler, EarlyStopping
+from keras.layers import Dense, LSTM, Input, Dropout, Bidirectional
+from keras.models import Sequential, Model
+from keras.callbacks import LearningRateScheduler
+from keras_self_attention import SeqSelfAttention
 from sklearn.preprocessing import MinMaxScaler
 from math import sqrt
 from sklearn.metrics import mean_squared_error
 
 matplotlib.use('TkAgg')
 
-attention = False
 print("Num GPUs Available: ", tf.config.list_physical_devices())
 
 sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True))
@@ -27,7 +33,8 @@ for device in gpu_devices:
     tf.config.experimental.set_memory_growth(device, True)
 
 
-def main():
+def main(attention, bidi, show):
+    start_time = time.time()
     # LOADING AND SCALING DATA ##
     dataframe = pd.read_csv("data/joined_data_2015-2019.csv",
                             parse_dates=['date'], index_col=1)
@@ -37,7 +44,7 @@ def main():
     dataframe = dataframe.reset_index()
     dataframe['WD'] = dataframe['date'].dt.dayofweek
     dataframe['MM'] = dataframe['date'].dt.month
-    variables = dataframe[["HH", "WD", "MM", "FH", "DD", "T", "AT"]]
+    variables = dataframe[["HH", "WD", "FH", "DD", "T", "AT"]]
     columns = list(variables)
     df = dataframe[columns].values.astype('float32')
     df = df[8784:, :]
@@ -59,24 +66,23 @@ def main():
 
     train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
     test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-    print(train_X.shape, train_Y.shape, test_X.shape, test_Y.shape)
 
     # MODEL
+    model = Sequential()
+    if bidi:
+        model.add(Bidirectional(LSTM(128, return_sequences=True), input_shape=(train_X.shape[1], train_X.shape[2], )))
+    else:
+        model.add(LSTM(128, input_shape=(train_X.shape[1], train_X.shape[2],),return_sequences=True))
 
     if attention:
-        inp = Input(shape=(train_X.shape[1], train_X.shape[2]))
-        x = ModelTrunk.ModelTrunk(num_layers=4, dropout=0.1)(inp)
-        x = Dense(1)(x)
-        x = Dense(1)(x)
+        model.add(SeqSelfAttention(attention_activation='sigmoid'))
 
-        model = Model(inp, x)
+    model.add(Dropout(0.1))
+    if bidi:
+        model.add(Bidirectional(LSTM(64)))
     else:
-        model = Sequential()
-        model.add(LSTM(128, input_shape=(train_X.shape[1], train_X.shape[2],),
-                  return_sequences=True))
-        model.add(Dropout(0.2))
         model.add(LSTM(64))
-        model.add(Dense(1))
+    model.add(Dense(1))
 
     model.compile(optimizer='adam', loss='mse')
     model.summary()
@@ -91,74 +97,120 @@ def main():
             return ((base_lr - min_lr) * pct) + min_lr
         return min_lr
 
-    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=20)
-    if attention:
-        callbacks = [LearningRateScheduler((lr_scheduler), verbose=1)]
-    else:
-        callbacks = [es]
+    callbacks = [LearningRateScheduler((lr_scheduler), verbose=1)]
 
-    history = model.fit(train_X, train_Y, epochs=200, batch_size=24,
+    history = model.fit(train_X, train_Y, epochs=200, batch_size=48,
               validation_data=(test_X, test_Y), verbose=1, shuffle=False, callbacks=callbacks)
 
-    if attention:
-        model.save_weights("models/attention.h5")
-    else:
-        model.save("models/lstm.h5")
-
-    # summarize history for loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
+    if show:
+        # summarize history for loss
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
 
     forecast = model.predict(test_X)
 
     test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
 
-    print(forecast.shape)
-    inv_forecast = np.concatenate((test_X[:, -6:], forecast), axis=1)
-    print(inv_forecast.shape)
+    inv_forecast = np.concatenate((test_X[:, -5:], forecast), axis=1)
     inv_forecast = scaler.inverse_transform(inv_forecast)
-    inv_forecast = inv_forecast[:, 6]
+    inv_forecast = inv_forecast[:, 5]
 
     test_Y = test_Y.reshape((len(test_Y), 1))
-    inv_y = np.concatenate((test_X[:, -6:], test_Y), axis=1)
+    inv_y = np.concatenate((test_X[:, -5:], test_Y), axis=1)
     inv_y = scaler.inverse_transform(inv_y)
-    inv_y = inv_y[:, 6]
+    inv_y = inv_y[:, 5]
+
+    def mean_absolute_percentage_error(y_true, y_pred):
+        y_true, y_pred = np.array(y_true), np.array(y_pred)
+        return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
     rmse = sqrt(mean_squared_error(inv_y, inv_forecast))
     reference_rmse = sqrt(mean_squared_error(inv_y,
                                              ref_forecasts[n_train_hours:]))
 
+    pcc = np.corrcoef(inv_y, inv_forecast)
+    reference_pcc = np.corrcoef(inv_y, ref_forecasts[n_train_hours:])
+
+    mape = mean_absolute_percentage_error(inv_y, inv_forecast)
+    reference_mape = mean_absolute_percentage_error(inv_y, ref_forecasts[n_train_hours:])
+
     currentTime = str(int(time.time()))
     if attention:
-        f = open("results/Test_results_attention_" + currentTime,
-                 "x")
+        f = open("results/Test_results_attention_" + currentTime, "x")
+    elif bidi:
+        f = open("results/Test_results_BiLSTM_" + currentTime, "x")
     else:
         f = open("results/Test_results_LSTM_" + currentTime, "x")
 
     print("written at " + currentTime)
     f.write('Test RMSE: %.3f \n' % rmse)
     f.write('Reference RMSE: %.3f \n' % reference_rmse)
+    f.write('Test PCC: %.3f \n' % pcc[0, 1])
+    f.write('Reference PCC: %.3f \n' % reference_pcc[0, 1])
+    f.write('Test MAPE: %.3f \n' % mape)
+    f.write('Reference MAPE: %.3f \n' % reference_mape)
 
     dataframe_graph = pd.DataFrame({'date': dates[n_train_hours:],
                                     'real': inv_y, 'forecast': inv_forecast,
                                     'ref_forecast':
                                     ref_forecasts[n_train_hours:]})
 
+    if attention and bidi:
+        dataframe_graph.to_csv('results/bilstm_attention/BiLSTM_prediction_'+ currentTime +'.csv')
+    elif attention:
+        dataframe_graph.to_csv('results/attention/Attention_prediction_' + currentTime +'.csv')
+    elif bidi:
+        dataframe_graph.to_csv('results/bilstm/BiLSTM_prediction_'+ currentTime + '.csv')
+    else:
+        dataframe_graph.to_csv('results/lstm/LSTM_prediction_'+ currentTime + '.csv')
+
     f.write(dataframe_graph.describe().to_string())
     f.close()
-    sns.lineplot(x=dataframe_graph['date'], y=dataframe_graph['real'],
-                 legend="full", label="Real [MW]")
-    sns.lineplot(x=dataframe_graph['date'], y=dataframe_graph['forecast'],
-                 legend="full", label="Forecast [MW]")
-    #sns.lineplot(x=dataframe_graph['date'], y=dataframe_graph['ref_forecast'],
-    #             legend="full", label="ref_Forecast [MW]")
-    plt.show()
+    if show:
+        sns.lineplot(x=dataframe_graph['date'], y=dataframe_graph['real'],
+                     legend="full", label="Real [MW]")
+        sns.lineplot(x=dataframe_graph['date'], y=dataframe_graph['forecast'],
+                     legend="full", label="Forecast [MW]")
+        plt.show()
 
+    return pcc, rmse, mape, (time.time()-start_time)
+
+def Average(lst):
+    return sum(lst)/len(lst)
+
+def Test(modelName, attention, bidi, times):
+    with open("aggresults-" + modelName, "x") as f:
+        pccArray, rmseArray, mapeArray, timeArray = [],[],[],[]
+        for i in range(times):
+            result_pcc, result_rmse, result_mape, result_time = main(attention, bidi, False)
+            pccArray.append(result_pcc[0,1])
+            rmseArray.append(result_rmse)
+            mapeArray.append(result_mape)
+            timeArray.append(result_time)
+
+        pcc = Average(pccArray)
+        rmse = Average(rmseArray)
+        mape = Average(mapeArray)
+        exec_time = Average(timeArray)
+
+        f.write(str(pccArray))
+        f.write(str(rmseArray))
+        f.write(str(mapeArray))
+        f.write(str(timeArray))
+
+        f.write(modelName + " agg pcc: %.6f \n" % pcc)
+        f.write(modelName + " agg RMSE: %.6f \n" % rmse)
+        f.write(modelName + " agg MAPE: %.6f \n" % mape)
+        f.write(modelName + " agg time: %.6f \n" % exec_time)
+        f.write("\n")
 
 if __name__ == "__main__":
-    main()
+    Test("AttentionBiLSTM", True, True, 1)
+    Test("AttentionLSTM", True, False, 1)
+    Test("BiLSTM", False, True, 1)
+    Test("LSTM", False, False, 1)
